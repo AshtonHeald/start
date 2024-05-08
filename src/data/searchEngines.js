0610028b891a82f9eababdf02b8f1e3fb07f1063
Ashton Heald <devtheash@gmail.com>
2024-05-08 13:31:13 -0500

    updated engines list

--- src/data/searchEngines.js
+++ src/data/searchEngines.js
@@ -1,9 +1,12 @@
 const searchEngines = [
   { name: 'Google', url: 'http://www.google.com/search?q=' },
   { name: 'Bing', url: 'https://www.bing.com/search?q='},
+  { name: 'Yahoo', url: 'https://search.yahoo.com/search?p='},
   { name: 'DuckDuckGo', url: 'https://duckduckgo.com/?q=' },
-  { name: 'Youtube', url: 'https://www.youtube.com/results?search_query=' },
+  { name: 'Youtube', url: 'https://www.youtube.com/results?search_query='},
   { name: 'Wikipedia', url: 'https://wikipedia.org/wiki/' },
+  { name: 'Wolfram Alpha', url: 'https://www.wolframalpha.com/input?i=' },
+  { name: 'Internet Archive', url: 'https://archive.org/search?query='},
 ];
 
 export default searchEngines;
