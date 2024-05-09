30169dbd2e4e72b79c0ab121522abff3fc2e38fe
Ashton Heald <devtheash@gmail.com>
2024-05-09 12:17:53 -0500

    uncommented added videos...

--- src/data/videoList.js
+++ src/data/videoList.js
@@ -1,24 +1,24 @@
 import robotWorkshop from "../assets/robot-workshop.mp4";
 import samuraiSkyView from "../assets/samurai-sky-view.mp4";
 import samuraiMatrix from "../assets/samurai-matrix.mp4";
-//import pixelDaytimeTrain from "../assets/pixel-daytime-train.mp4";
-//import pixelMidnightTrain from "../assets/pixel-nighttime-train.mp4";
+import pixelDaytimeTrain from "../assets/pixel-daytime-train.mp4";
+import pixelMidnightTrain from "../assets/pixel-nighttime-train.mp4";
 import pixelDVaRoom from "../assets/pixel-d.va-room.mp4";
 import hudInterface from "../assets/hud-interface.mp4"
 import flowerGirl from "../assets/flower-girl.mp4";
-//import lofiGirl from "../assets/lofi-girl.mp4";
+import lofiGirl from "../assets/lofi-girl.mp4";
 
 const videoList = [
   { url: '', title: 'None'},
   { url: robotWorkshop, title: 'Robot Workshop' },
   { url: samuraiMatrix, title: 'Samurai Matrix' },
   { url: samuraiSkyView, title: 'Samurai Sky View' },
-  //{ url: pixelDaytimeTrain, title: 'Pixel Daytime Train' },
-  //{ url: pixelMidnightTrain, title: 'Pixel Midnight Train' },
+  { url: pixelDaytimeTrain, title: 'Pixel Daytime Train' },
+  { url: pixelMidnightTrain, title: 'Pixel Midnight Train' },
   { url: pixelDVaRoom, title: 'Pixel D.Va Room' },
   { url: hudInterface, title: 'Hud Interface' },
   { url: flowerGirl, title: 'Flower Girl' },
-  //{ url: lofiGirl, title: 'Lofi Girl' },
+  { url: lofiGirl, title: 'Lofi Girl' },
   // Add more video URLs and titles as needed
 ];
 
